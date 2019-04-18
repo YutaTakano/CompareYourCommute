518f96359839a99a468be55e1ed8f6827c050a76
Yuta Takano <yutatakano@aol.com>
2019-04-18 14:42:38 -0400

    Modified code to sort by travel time. Added error message for javascript disabled browsers.

--- CompareYourCommute/routes/index.js
+++ CompareYourCommute/routes/index.js
@@ -32,74 +32,7 @@ router.get('/', function(req,res,next) {
 
 });
 
-/*----------------------------------------------------------------------*/
 
-//
-// /* POST to get the form data from page */
-// router.post('/', function (req, res) {
-//
-//     /* To store durations of each travel method */
-//     let durations = [];
-//     /* To store JSON object from API call */
-//     let result = '';
-//
-//     /* Input form data for start and end addresses */
-//     const inputdata =
-//     {
-//         start: req.body.start_address,
-//         destination: req.body.end_address
-//     };
-//
-//     /* Travel modes to be passed into Google's API */
-//     const google_travel_modes =
-//         [
-//             'Walking',
-//             'Driving',
-//             'Cycling',
-//             'Transit'
-//         ];
-//
-//
-//     /* Post request to the directions API */
-//     const options = { method: 'POST',
-//         url: 'https://maps.googleapis.com/maps/api/directions/json',
-//         qs:
-//             { origin: inputdata.start,
-//                 destination: inputdata.destination,
-//                 key: GOOGLE_DIRECTIONS},
-//         headers:
-//             { 'Postman-Token': '933760e5-db5b-41e9-93f7-e7ec0a1c45d6',
-//                 'cache-control': 'no-cache' } };
-//
-//     request(options, function (error, response, inhalt) {
-//         if (error) throw new Error(error);
-//
-//
-//         /* Parse the data returned from API call for duration, start, and end address */
-//
-//         try {
-//             result = JSON.parse(inhalt);
-//         }
-//
-//         catch (err){
-//             console.log("GOOGLE API PARSE ERROR");
-//             res.render('error', {message: 'API Error'});
-//         }
-//
-//
-//         durations.push(result.routes[0].legs[0].duration.text);
-//         const start = result.routes[0].legs[0].start_address;
-//         const end = result.routes[0].legs[0].end_address;
-//         res.render('results', {title: 'Compare Your Commute', duration: durations, start: start, end: end, mode: google_travel_modes});
-//
-//
-//     });
-//
-//
-//
-//
-//
-// });
 
 // -------------------------------------------------------------------------------------------------------
 router.post('/', function (req, res) {
@@ -122,20 +55,11 @@ router.post('/', function (req, res) {
             destination: req.body.end_address
         };
 
-    /* Travel modes to be passed into Google's API */
-    const google_travel_modes =
-        [
-            'Walking',
-            'Driving',
-            'Cycling',
-            'Transit'
-        ];
 
 
     function walking_api () {
         return new Promise (function (resolve, reject) {
 
-            //if (error) {reject(error + ' Walking Promise rejected');}
 
             /* Post request setup to the directions API */
             const options = {
@@ -164,12 +88,14 @@ router.post('/', function (req, res) {
                 let result = JSON.parse(inhalt);
                 start = result.routes[0].legs[0].start_address;
                 end = result.routes[0].legs[0].end_address;
-                durations.push(result.routes[0].legs[0].duration.text);
-                modes.push(result.routes[0].legs[0].steps[0].travel_mode);
 
 
+                let travel_mode = (result.routes[0].legs[0].steps[0].travel_mode);
+                let travel_secs = (result.routes[0].legs[0].duration.value); // travel time in seconds, need to standardize to compare
+                let travel_time = (result.routes[0].legs[0].duration.text);
 
-                    //data.walking = {}
+                data.push([[travel_mode],[travel_time],[travel_secs]]);
+
             })
         })
             .catch((err) => {
@@ -207,10 +133,12 @@ router.post('/', function (req, res) {
                 console.log(inhalt);
 
                 let result = JSON.parse(inhalt);
-                durations.push(result.routes[0].legs[0].duration.text);
-                modes.push(result.routes[0].legs[0].steps[0].travel_mode);
 
+                let travel_mode = (result.routes[0].legs[0].steps[0].travel_mode);
+                let travel_secs = (result.routes[0].legs[0].duration.value); // travel time in seconds, need to standardize to compare
+                let travel_time = (result.routes[0].legs[0].duration.text);
 
+                data.push([[travel_mode],[travel_time],[travel_secs]]);
 
 
             })
@@ -252,12 +180,10 @@ router.post('/', function (req, res) {
                 let result = JSON.parse(inhalt);
 
                 let travel_mode = (result.routes[0].legs[0].steps[0].travel_mode);
+                let travel_secs = (result.routes[0].legs[0].duration.value); // travel time in seconds, need to standardize to compare
                 let travel_time = (result.routes[0].legs[0].duration.text);
 
-                data.push([travel_mode, travel_time]);
-
-                durations.push(travel_time);
-                modes.push(travel_mode);
+                data.push([[travel_mode],[travel_time],[travel_secs]]);
 
             })
         })
@@ -266,15 +192,27 @@ router.post('/', function (req, res) {
             });
     }
 
+    /* ------------------------------------- */
+
     const walking = walking_api();
     const driving = driving_api();
     const cycling = cycling_api();
 
+
+    // This promise.all gathers all the data from API calls, sorts the data in order, and spits it out in a res.render
+
     Promise.all([walking,cycling,driving])
         .then(function(err,response){
 
-        data = data.sort(([a], [b]) => a < b ? -1 : a > b ? 1 : 0);
-        
+
+            data = data.sort(function(a,b){return a[2] < b[2];}); // Sorts the data by travel time increasing
+            console.log(data);
+
+            for (let i = 0; i < data.length; i++) {
+                modes.push(data[i][0]);
+                durations.push(data[i][1]);
+            }
+
         res.render('results', {
             title: 'Compare Your Commute',
             duration: durations,
@@ -283,8 +221,6 @@ router.post('/', function (req, res) {
             mode: modes
         });
     })
-
-
 
 
 });
